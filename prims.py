7d10bf51a4f881454b1707da0e303d86e8d3541e
labexams <86876820+labexams@users.noreply.github.com>
2021-12-22 14:19:41 +0530

    Update prims.py

--- prims.py
+++ prims.py
@@ -1,4 +1,4 @@
-import sys 
+import sys
 
 class Graph():
     def __init__(self, vertices):
@@ -10,27 +10,26 @@ class Graph():
         for i in range(1, self.V):
             print(parent[i], "-", i, "\t", self.graph[i][parent[i]])
 
-    def minKey(self, key, mstSet):
-
+    def mindist(self, dist, sptSet):
         min = sys.maxsize
         for v in range(self.V):
-            if key[v] < min and mstSet[v] == False:
-                min = key[v]
+            if dist[v] < min and sptSet[v] == False:
+                min = dist[v]
                 min_index = v
         return min_index
 
-    def primMST(self):     
-        key = [sys.maxsize] * self.V
-        parent = [None] * self.V  
-        key[0] = 0
-        mstSet = [False] * self.V
-        parent[0] = -1 
+    def primMST(self):
+        dist = [sys.maxsize] * self.V
+        parent = [None] * self.V
+        dist[0] = 0
+        sptSet = [False] * self.V
+        parent[0] = -1
         for cout in range(self.V):
-            u = self.minKey(key, mstSet)
-            mstSet[u] = True
+            u = self.mindist(dist, sptSet)
+            sptSet[u] = True
             for v in range(self.V):
-                if self.graph[u][v] > 0 and mstSet[v] == False and key[v] > self.graph[u][v]:
-                    key[v] = self.graph[u][v]
+                if self.graph[u][v] > 0 and sptSet[v] == False and dist[v] > self.graph[u][v]:
+                    dist[v] = self.graph[u][v]
                     parent[v] = u
         self.printMST(parent)
 
