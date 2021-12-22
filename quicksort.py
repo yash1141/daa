d71e42a74bbecb5ab915dbfca384f5e87fe9c48e
labexams <86876820+labexams@users.noreply.github.com>
2021-12-22 15:49:32 +0530

    Update quicksort.py

--- quicksort.py
+++ quicksort.py
@@ -1,8 +1,6 @@
 def swap(a, b, arr):
-    if a!=b:
-        tmp = arr[a]
-        arr[a] = arr[b]
-        arr[b] = tmp
+    arr[a], arr[b] = arr[b], arr[a]
+
 
 def quick_sort(elements, start, end):
     if start < end:
@@ -10,16 +8,17 @@ def quick_sort(elements, start, end):
         quick_sort(elements, start, pi-1)
         quick_sort(elements, pi+1, end)
 
+
 def partition(elements, start, end):
     pivot_index = start
     pivot = elements[pivot_index]
 
     while start < end:
         while start < len(elements) and elements[start] <= pivot:
-            start+=1
-
+            start += 1
+            
         while elements[end] > pivot:
-            end-=1
+            end -= 1
 
         if start < end:
             swap(start, end, elements)
@@ -27,6 +26,7 @@ def partition(elements, start, end):
     swap(pivot_index, end, elements)
 
     return end
+
 
 lst = [10, 3, 15, 7, 8, 23, 98, 29]
 quick_sort(lst,0,len(lst)-1)
