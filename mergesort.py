364478ce14b8308c8546b0d1715ff65daf254e92
labexams <86876820+labexams@users.noreply.github.com>
2021-12-22 13:24:49 +0530

    Create mergesort.py

---
+++ mergesort.py
@@ -0,0 +1,42 @@
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return
+
+    mid = len(arr)//2
+
+    left = arr[:mid]
+    right = arr[mid:]
+
+    merge_sort(left)
+    merge_sort(right)
+
+    merge_two_sorted_lists(left, right, arr)
+
+def merge_two_sorted_lists(a,b,arr):
+    len_a = len(a)
+    len_b = len(b)
+
+    i = j = k = 0
+
+    while i < len_a and j < len_b:
+        if a[i] <= b[j]:
+            arr[k] = a[i]
+            i+=1
+        else:
+            arr[k] = b[j]
+            j+=1
+        k+=1
+
+    while i < len_a:
+        arr[k] = a[i]
+        i+=1
+        k+=1
+
+    while j < len_b:
+        arr[k] = b[j]
+        j+=1
+        k+=1
+
+lst = [10, 3, 15, 7, 8, 23, 98, 29]
+merge_sort(lst)
+print(lst)
