f59dc1b3fd8ce65992d95c44efae9f4c96654336
labexams <86876820+labexams@users.noreply.github.com>
2021-12-22 22:16:59 +0530

    Update dijkstra.py

--- dijkstra.py
+++ dijkstra.py
@@ -1,12 +1,13 @@
 import sys
 
+
 def printSolution(dist):
-    print("Vertex tDistance from Source")
-    for node in range(V):
-        print(node, "t", dist[node])
+    print("Vertex Distance from Source")
+    for node in range(1,V):
+        print("Distance of ", node, "is", dist[node])
 
 
-def minDistance(dist, sptSet):
+def mindist(dist, sptSet):
     min = sys.maxsize
     for v in range(V):
         if dist[v] < min and sptSet[v] == False:
@@ -21,7 +22,7 @@ def dijkstra(src):
     sptSet = [False] * V
 
     for _ in range(V):
-        u = minDistance(dist, sptSet)
+        u = mindist(dist, sptSet)
         sptSet[u] = True
         for v in range(V):
             if graph[u][v] > 0 and sptSet[v] == False and dist[v] > dist[u] + graph[u][v]:
@@ -31,10 +32,10 @@ def dijkstra(src):
 
 V = 5
 graph = [[0, 10, 0, 30, 100],
-           [10, 0, 50, 0, 0],
-           [0, 50, 0, 20, 10],
-           [30, 0, 20, 0, 60],
-           [100, 0, 10, 60, 0]
-           ]
+         [10, 0, 50, 0, 0],
+         [0, 50, 0, 20, 10],
+         [30, 0, 20, 0, 60],
+         [100, 0, 10, 60, 0]
+         ]
 
 dijkstra(0)
